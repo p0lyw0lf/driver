889391b716c027538869c883234f6ee575e7a4fa
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2026-03-14 16:59:34 -0400

    get information for relative file reads
    this was quite the saga. first I had to submit a patch to boa upstream
    (relatively painless tbh, got accepted very quickly!) but then trying to
    use my patch as a git dependency led me down a rabbit hole of "oh is
    cargo broken?" to "OH IS GIT BROKEN??", finally bottoming out at "ah wth
    why does `init.defaultBranch = main` have anything to do with this"
    (setting it to `master` allowed the clone to work). anyways it's time to
    actually test the things now.

--- query/src/js/path.rs
+++ query/src/js/path.rs
@@ -1,29 +1,32 @@
 use std::path::PathBuf;
 
-use boa_engine::{JsError, JsNativeError};
-use boa_engine::{JsResult, value::TryFromJs};
+use boa_engine::{Context, JsError, JsNativeError, JsResult, value::TryFromJs};
 use relative_path::RelativePath;
 use relative_path::RelativePathBuf;
 
 // SHOULD be called from a Javascript callback
-fn get_current_file() -> JsResult<PathBuf> {
-    todo!("need to add the current filename to the overall QUERY_CONTEXT");
+fn get_current_file(js_ctx: &mut Context) -> JsResult<PathBuf> {
+    Ok(js_ctx
+        .get_active_script_or_module()
+        .ok_or_else(|| {
+            JsNativeError::eval().with_message("must be run from within a script/module")
+        })?
+        .path()
+        .ok_or_else(|| JsNativeError::eval().with_message("script/module doesn't have filename"))?
+        .to_path_buf())
 }
 
 /// Helper struct that parses a path relative to the current file itno a path relative to the cwd.
 pub struct JsPath(pub PathBuf);
 
 impl TryFromJs for JsPath {
-    fn try_from_js(
-        value: &boa_engine::JsValue,
-        _js_ctx: &mut boa_engine::Context,
-    ) -> JsResult<Self> {
+    fn try_from_js(value: &boa_engine::JsValue, js_ctx: &mut Context) -> JsResult<Self> {
         let path = value
             .as_string()
             .ok_or_else(|| JsNativeError::typ().with_message("path must be string"))?
             .to_std_string()
             .map_err(JsError::from_rust)?;
-        let base_file = get_current_file()?;
+        let base_file = get_current_file(js_ctx)?;
         let base_directory = base_file
             .parent()
             .ok_or_else(|| JsNativeError::eval().with_message("no parent directory??"))?;
