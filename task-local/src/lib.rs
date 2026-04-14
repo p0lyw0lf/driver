540b56d89d36c4083358b0796526f76ea75ea730
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2026-04-14 16:01:19 -0400

    better strategy for task-local
    this puts more code outside the macro, in generics. in preparation for a
    blog post so I can reference this code

--- task-local/src/lib.rs
+++ task-local/src/lib.rs
@@ -1,73 +1,76 @@
-pub use pastey::paste;
+use std::cell::RefCell;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::thread::LocalKey;
+
+/// Wrapper future that stores a previous context to the stack, if there is any.
+pub struct Scoped<F: Future, T: 'static> {
+    tls: &'static LocalKey<RefCell<Option<T>>>,
+    curr: Option<T>,
+    fut: F,
+}
+
+impl<F: Future, T: 'static> Future for Scoped<F, T> {
+    type Output = F::Output;
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        // SAFETY: self stays pinned
+        let this = unsafe { &mut self.get_unchecked_mut() };
+        // SAFETY: because self is pinned, this is pinned
+        let fut = unsafe { Pin::new_unchecked(&mut this.fut) };
+        let curr = &mut this.curr;
+        let mut swap = || this.tls.with_borrow_mut(|prev| std::mem::swap(curr, prev));
+
+        swap();
+        let out = fut.poll(cx);
+        swap();
+
+        out
+    }
+}
+
+impl<F: Future, T: 'static> Scoped<F, T> {
+    /// Call this to get the value out of the scope once you're done polling the
+    /// future.
+    pub fn take_value(self: Pin<&mut Self>) -> Option<T> {
+        // SAFETY: self stays pinned
+        let this = unsafe { &mut self.get_unchecked_mut() };
+        this.curr.take()
+    }
+}
+
+pub struct ScopeBuilder<T: 'static> {
+    tls: &'static LocalKey<RefCell<Option<T>>>,
+}
+impl<T: 'static> ScopeBuilder<T> {
+    pub const fn new(tls: &'static LocalKey<RefCell<Option<T>>>) -> Self {
+        Self { tls }
+    }
+
+    pub fn scope<F: Future>(&self, value: T, fut: F) -> Scoped<F, T> {
+        Scoped {
+            tls: self.tls,
+            curr: Some(value),
+            fut,
+        }
+    }
+
+    pub fn with<V>(&self, f: impl FnOnce(Option<&T>) -> V) -> V {
+        self.tls.with_borrow(|value| f(value.as_ref()))
+    }
+
+    pub fn with_mut<V>(&self, f: impl FnOnce(Option<&mut T>) -> V) -> V {
+        self.tls.with_borrow_mut(|value| f(value.as_mut()))
+    }
+}
 
 #[macro_export]
 macro_rules! task_local {
-    ($(static $ident:ident : $ty:ty ;)*) => {$( $crate::paste! {
-        mod [<def_ $ident:lower:snake>] {
-            use super::*;
-            use std::cell::RefCell;
-            use std::pin::Pin;
-            use std::task::{Context, Poll};
-
+    ($(static $ident:ident : $ty:ty ;)*) => {$(
+        static $ident: $crate::ScopeBuilder<$ty> = {
             std::thread_local! {
-                /// Thread-local that only gets set while the associated task is actively being
-                /// polled.
-                static LOCAL: RefCell<Option<$ty>> = const { RefCell::new(None) };
+                static LOCAL: std::cell::RefCell<Option<$ty>> = const { std::cell::RefCell::new(None) };
             }
-
-            /// Wrapper future that stores a previous context to the stack, if there is any.
-            pub struct Scoped<F: Future> {
-                curr: Option<$ty>,
-                fut: F,
-            }
-
-            impl<F: Future> Future for Scoped<F> {
-                type Output = F::Output;
-                fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-                    // SAFETY: self stays pinned
-                    let this = unsafe { &mut self.get_unchecked_mut() };
-                    // SAFETY: because self is pinned, this is pinned
-                    let fut = unsafe { Pin::new_unchecked(&mut this.fut) };
-                    let curr = &mut this.curr;
-                    let mut swap = || LOCAL.with_borrow_mut(|prev| std::mem::swap(curr, prev));
-
-                    swap();
-                    let out = fut.poll(cx);
-                    swap();
-
-                    out
-                }
-            }
-
-            impl<F: Future> Scoped<F> {
-                /// Call this to get the value out of the scope once you're done polling the
-                /// future.
-                pub fn take_value(self: Pin<&mut Self>) -> Option<$ty> {
-                    // SAFETY: self stays pinned
-                    let this = unsafe { &mut self.get_unchecked_mut() };
-                    this.curr.take()
-                }
-            }
-
-            pub struct ScopeBuilder;
-            impl ScopeBuilder {
-                pub fn scope<F: Future>(&self, value: $ty, fut: F) -> Scoped<F> {
-                    Scoped {
-                        curr: Some(value),
-                        fut,
-                    }
-                }
-
-                pub fn with<T>(&self, f: impl FnOnce(Option<&$ty>) -> T) -> T {
-                    LOCAL.with_borrow(|value| f(value.as_ref()))
-                }
-
-                pub fn with_mut<T>(&self, f: impl FnOnce(Option<&mut $ty>) -> T) -> T {
-                    LOCAL.with_borrow_mut(|value| f(value.as_mut()))
-                }
-            }
-        }
-
-        static $ident: [<def_ $ident:lower:snake>]::ScopeBuilder = [<def_ $ident:lower:snake>]::ScopeBuilder;
-    } )*};
+            $crate::ScopeBuilder::new(&LOCAL)
+        };
+    )*}
 }
