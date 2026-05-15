8fde34185be9b5846188f290c734efb574f97b0f
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2026-05-15 10:58:25 -0400

    overhaul markdown_to_html
    now supports KaTeX & table-wrapper generation. also refactors how the
    options are made so we don't need to have as many thread locals as
    before.

--- query/src/query/html.rs
+++ query/src/query/html.rs
@@ -7,20 +7,30 @@ use crate::{
 
 query_key!(MarkdownToHtml(pub Object););
 
-impl Producer for MarkdownToHtml {
-    type Output = crate::Result<Object>;
+struct Options {
+    comrak_options: comrak::Options<'static>,
+    comrak_plugins: comrak::options::Plugins<'static>,
+    katex_ctx: katex::KatexContext,
+    katex_settings: katex::Settings,
+}
 
-    #[tracing::instrument(level = "debug", skip_all)]
-    async fn produce(&self, ctx: &QueryContext) -> Self::Output {
-        let contents = self.0.contents_as_string(ctx)?;
-
+impl Default for Options {
+    fn default() -> Self {
         thread_local! {
-            static OPTIONS: comrak::Options<'static> = comrak::Options {
+            static HIGHLIGHTER: arborium::Highlighter = arborium::Highlighter::with_config(arborium::Config {
+                html_format: arborium::HtmlFormat::ClassNames,
+                ..Default::default()
+            });
+        }
+
+        Self {
+            comrak_options: comrak::Options {
                 extension: comrak::options::Extension::builder()
                     .strikethrough(true)
                     .table(true)
                     .autolink(false)
                     .tasklist(true)
+                    .header_id_prefix("heading-".to_string())
                     .superscript(false)
                     .subscript(false)
                     .footnotes(true)
@@ -43,79 +53,67 @@ impl Producer for MarkdownToHtml {
                     .escape(false)
                     .tasklist_classes(true)
                     .build(),
-            };
-            static HIGHLIGHTER: arborium::Highlighter = arborium::Highlighter::with_config(arborium::Config {
-                html_format: arborium::HtmlFormat::ClassNames,
-                ..Default::default()
-            });
+            },
+            comrak_plugins: comrak::options::Plugins::builder()
+                .render(comrak::options::RenderPlugins {
+                    codefence_renderers: Default::default(),
+                    codefence_syntax_highlighter: Some(&ArboriumHighlighter(&HIGHLIGHTER)),
+                    heading_adapter: None,
+                })
+                .build(),
+            katex_ctx: katex::KatexContext::default(),
+            katex_settings: katex::Settings::default(),
+        }
+    }
+}
 
+impl Producer for MarkdownToHtml {
+    type Output = crate::Result<Object>;
+
+    #[tracing::instrument(level = "debug", skip_all)]
+    async fn produce(&self, ctx: &QueryContext) -> Self::Output {
+        let contents = self.0.contents_as_string(ctx)?;
+
+        thread_local! {
+            static OPTIONS: Options = Options::default();
         }
 
-        /// Very silly things I have to do to get thread locals to reference properly...
-        struct ArboriumHighlighter(&'static std::thread::LocalKey<arborium::Highlighter>);
-        impl comrak::adapters::SyntaxHighlighterAdapter for ArboriumHighlighter {
-            fn write_highlighted(
-                &self,
-                output: &mut dyn std::fmt::Write,
-                lang: Option<&str>,
-                code: &str,
-            ) -> std::fmt::Result {
-                match lang {
-                    None => comrak::html::escape(output, code),
-                    Some(lang) => {
-                        if lang.is_empty() {
-                            comrak::html::escape(output, code)
-                        } else {
-                            let mut highlighter = self.0.with(|h| h.fork());
-                            match highlighter.highlight(lang, code).map_err(|e| {
-                                eprintln!("error highlighting code: {e}");
-                                std::fmt::Error
-                            }) {
-                                Ok(html) => output.write_str(&html),
-                                Err(_) => comrak::html::escape(output, code),
-                            }
-                        }
-                    }
+        let output = OPTIONS.with(|options| -> crate::Result<_> {
+            let arena = comrak::Arena::new();
+            let root = comrak::parse_document(&arena, &contents, &options.comrak_options);
+
+            for node in root.descendants() {
+                let node_value = &mut node.data_mut().value;
+                if let comrak::nodes::NodeValue::Math(node_math) = node_value {
+                    *node_value =
+                        comrak::nodes::NodeValue::HtmlBlock(comrak::nodes::NodeHtmlBlock {
+                            // TODO: I have no idea what this is supposed to be, hope it doesn't
+                            // matter lol
+                            block_type: 0,
+                            literal: katex::render_to_string(
+                                &options.katex_ctx,
+                                &node_math.literal,
+                                &options.katex_settings,
+                            )?,
+                        });
                 }
             }
 
-            fn write_pre_tag(
-                &self,
-                output: &mut dyn std::fmt::Write,
-                _attributes: std::collections::HashMap<&'static str, std::borrow::Cow<'_, str>>,
-            ) -> std::fmt::Result {
-                comrak::html::write_opening_tag(
-                    output,
-                    "pre",
-                    vec![("class", "syntax-highlighting")],
-                )
-            }
+            let mut out = String::new();
+            comrak::html::format_document_with_plugins(
+                root,
+                &options.comrak_options,
+                &mut out,
+                &options.comrak_plugins,
+            )?;
 
-            fn write_code_tag(
-                &self,
-                output: &mut dyn std::fmt::Write,
-                attributes: std::collections::HashMap<&'static str, std::borrow::Cow<'_, str>>,
-            ) -> std::fmt::Result {
-                comrak::html::write_opening_tag(output, "code", attributes)
-            }
-        }
+            // Doing this here instead of in javascript for a slight bit of extra perf :)
+            out = out
+                .replace("<table>", "<div class=table-wrapper><table>")
+                .replace("</table>", "</table></div>");
 
-        thread_local! {
-            static PLUGINS: comrak::options::Plugins<'static> = comrak::options::Plugins::builder()
-                .render(comrak::options::RenderPlugins {
-                    codefence_renderers: Default::default(),
-                    codefence_syntax_highlighter: Some(
-                        &ArboriumHighlighter(&HIGHLIGHTER),
-                    ),
-                    heading_adapter: None,
-                })
-                .build();
-        }
-
-        let output = OPTIONS.with(|options| {
-            PLUGINS
-                .with(|plugins| comrak::markdown_to_html_with_plugins(&contents, options, plugins))
-        });
+            Ok(out)
+        })?;
 
         let object = ctx.db().objects.store(output.into_bytes())?;
         Ok(object)
@@ -141,5 +139,50 @@ impl Producer for MinifyHtml {
         let output = minify_html::minify(contents.as_bytes(), &cfg);
         let object = ctx.db().objects.store(output)?;
         Ok(object)
+    }
+}
+
+/// Very silly things I have to do to get thread locals to reference properly...
+struct ArboriumHighlighter(&'static std::thread::LocalKey<arborium::Highlighter>);
+impl comrak::adapters::SyntaxHighlighterAdapter for ArboriumHighlighter {
+    fn write_highlighted(
+        &self,
+        output: &mut dyn std::fmt::Write,
+        lang: Option<&str>,
+        code: &str,
+    ) -> std::fmt::Result {
+        match lang {
+            None => comrak::html::escape(output, code),
+            Some(lang) => {
+                if lang.is_empty() {
+                    comrak::html::escape(output, code)
+                } else {
+                    let mut highlighter = self.0.with(|h| h.fork());
+                    match highlighter.highlight(lang, code).map_err(|e| {
+                        eprintln!("error highlighting code: {e}");
+                        std::fmt::Error
+                    }) {
+                        Ok(html) => output.write_str(&html),
+                        Err(_) => comrak::html::escape(output, code),
+                    }
+                }
+            }
+        }
+    }
+
+    fn write_pre_tag(
+        &self,
+        output: &mut dyn std::fmt::Write,
+        _attributes: std::collections::HashMap<&'static str, std::borrow::Cow<'_, str>>,
+    ) -> std::fmt::Result {
+        comrak::html::write_opening_tag(output, "pre", vec![("class", "syntax-highlighting")])
+    }
+
+    fn write_code_tag(
+        &self,
+        output: &mut dyn std::fmt::Write,
+        attributes: std::collections::HashMap<&'static str, std::borrow::Cow<'_, str>>,
+    ) -> std::fmt::Result {
+        comrak::html::write_opening_tag(output, "code", attributes)
     }
 }
