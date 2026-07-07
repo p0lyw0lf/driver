dd10c19637d26a7b396b92774d51afdb232edf86
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2026-07-06 22:09:39 -0400

    wip: first pass tera conversion
    there really have been a lot of breaking changes with the new major
    release huh. I've had to patch around at least one thing so far, might
    be others but we'll see. so far so good

--- packages/driver-query-ssg/src/tera/mod.rs
+++ packages/driver-query-ssg/src/tera/mod.rs
@@ -6,7 +6,7 @@ use std::task::Poll;
 use futures_lite::future;
 use relative_path::{RelativePath, RelativePathBuf};
 use serde::{Deserialize, Serialize};
-use tera::Tera;
+use tera::{Tera, TeraResult};
 
 use driver_engine::{Blob, query};
 use driver_query_fs::{ListDirectory, ReadFile};
@@ -131,53 +131,24 @@ fn render_tera(ctx: &QueryContext, input: &Blob, file: &Path, arg: &JsValue) -> 
     RunTeraOutput { export, writes }
 }
 
-macro_rules! get_arg {
-    ($name:ident <- $args: expr) => {
-        let $name = $args.get(stringify!($name)).ok_or_else(|| {
-            tera::Error::from(concat!(
-                "\"",
-                stringify!($name),
-                "\" parameter must be specified"
-            ))
-        })?;
-    };
-    ($name:ident : $fn:ident <- $args: expr) => {
-        let $name = $args
-            .get(stringify!($name))
-            .ok_or_else(|| {
-                tera::Error::from(concat!(
-                    "\"",
-                    stringify!($name),
-                    "\" parameter must be specified"
-                ))
-            })?
-            .$fn()
-            .ok_or_else(|| {
-                tera::Error::from(concat!("\"", stringify!($name), "\" must be a string"))
-            })?;
-    };
-}
-
 fn register_functions(tera: &mut Tera, ctx: &QueryContext, writes: Arc<Mutex<WriteOutput>>) {
     macro_rules! wrap_function {
         (move ($($i:ident),*) |$args:ident| $body:tt) => {{
             $(
                 let $i = $i.clone();
             )*
-            move |$args: &HashMap<String, tera::Value>| -> tera::Result<tera::Value> {
+            move |$args: tera::Kwargs, _state| -> TeraResult<tera::Value> {
                 $body
             }
         }};
     }
 
     macro_rules! wrap_filter {
-        (move ($($i:ident),*) |$arg:ident, $args:ident| $body:tt) => {{
+        (move ($($i:ident),*) |$arg:ident: $ty:ty| $body:tt) => {{
             $(
                 let $i = $i.clone();
             )*
-            move |$arg: &tera::Value,
-                  $args: &HashMap<String, tera::Value>|
-                  -> tera::Result<tera::Value> {
+            move |$arg: $ty, _args, _state| -> TeraResult<tera::Value> {
                 $body
             }
         }};
@@ -186,10 +157,10 @@ fn register_functions(tera: &mut Tera, ctx: &QueryContext, writes: Arc<Mutex<Wri
     tera.register_function(
         "read",
         wrap_function!(move(ctx) |args| {
-            get_arg!(file: as_str <- args);
+            let file: &str = args.must_get("file")?;
             let file = resolve_path(file)?;
             let read_file = ReadFile(file);
-            let blob = future::block_on(query(&ctx, read_file.clone())).map_err(|e| format!("{read_file}: {e}"))?;
+            let blob = future::block_on(query(&ctx, read_file.clone())).map_err(|e| tera::Error::message(format!("{read_file}: {e}")))?;
             js_to_tera_value(&JsValue::Store(JsBlob { blob }))
         }),
     );
@@ -197,27 +168,26 @@ fn register_functions(tera: &mut Tera, ctx: &QueryContext, writes: Arc<Mutex<Wri
     tera.register_function(
         "list",
         wrap_function!(move(ctx) |args| {
-                get_arg!(dir: as_str <- args);
-                let dir = resolve_path(dir)?;
-                let list_directory = ListDirectory(dir);
-                let files =
-                    future::block_on(query(&ctx, list_directory.clone())).map_err(|e| format!("{list_directory}: {e}"))?;
-                Ok(files
-                    .into_iter()
-                    .map(|f| format!("{}", f.display()))
-                    .collect::<Vec<String>>()
-                    .into())
-            }
-        ),
+            let dir: &str = args.must_get("dir")?;
+            let dir = resolve_path(dir)?;
+            let list_directory = ListDirectory(dir);
+            let files =
+                future::block_on(query(&ctx, list_directory.clone())).map_err(|e| tera::Error::message(format!("{list_directory}: {e}")))?;
+            Ok(files
+                .into_iter()
+                .map(|f| format!("{}", f.display()))
+                .collect::<Vec<String>>()
+                .into())
+        }),
     );
 
     tera.register_function(
         "file_type",
         wrap_function!(move() |args| {
-            get_arg!(entry: as_str <- args);
+            let entry: &str = args.must_get("entry")?;
             let entry = resolve_path(entry)?;
 
-            let metadata = std::fs::metadata(entry).map_err(|e| e.to_string())?;
+            let metadata = std::fs::metadata(entry)?;
 
             Ok(if metadata.is_file() {
                 "file"
@@ -235,7 +205,7 @@ fn register_functions(tera: &mut Tera, ctx: &QueryContext, writes: Arc<Mutex<Wri
     tera.register_function(
         "run_js",
         wrap_function!(move(ctx, writes) |args| {
-            get_arg!(file: as_str <- args);
+            let file: &str = args.must_get("file")?;
             let file = resolve_path(file)?;
             let arg = tera_to_js_context(args, "file")?;
 
@@ -251,7 +221,7 @@ fn register_functions(tera: &mut Tera, ctx: &QueryContext, writes: Arc<Mutex<Wri
             }
             let output = js_to_tera_value(
                 &output.export
-                .map_err(|e| format!("{run_js}:\n\t{e}"))?
+                .map_err(|e| tera::Error::message(format!("{run_js}:\n\t{e}")))?
             )?;
 
             Ok(output)
@@ -261,7 +231,7 @@ fn register_functions(tera: &mut Tera, ctx: &QueryContext, writes: Arc<Mutex<Wri
     tera.register_function(
         "run_tera",
         wrap_function!(move(ctx, writes) |args| {
-            get_arg!(template: as_str <- args);
+            let template: &str = args.must_get("template")?;
             let file = resolve_path(template)?;
             let arg = tera_to_js_context(args, "template")?;
 
@@ -274,7 +244,7 @@ fn register_functions(tera: &mut Tera, ctx: &QueryContext, writes: Arc<Mutex<Wri
                 writes.lock().unwrap().merge(output.writes);
             };
             let output = js_to_tera_value(&JsValue::Store(JsBlob {
-                blob: output.export.map_err(|e| format!("{run_tera}:\n\t{e}"))?,
+                blob: output.export.map_err(|e| tera::Error::message(format!("{run_tera}:\n\t{e}")))?,
             }))?;
 
             Ok(output)
@@ -283,36 +253,30 @@ fn register_functions(tera: &mut Tera, ctx: &QueryContext, writes: Arc<Mutex<Wri
 
     tera.register_filter(
         "store",
-        wrap_filter!(move(ctx) |arg, _args| {
-            let tera::Value::String(s) = arg else {
-                return Err("store must take in str".into());
-            };
+        wrap_filter!(move(ctx) |arg: &str| {
             let blob = ctx
-                .store(s.clone().into_bytes())
-                .map_err(|e| e.to_string())?;
+                .store(String::from(arg).into_bytes())
+                .map_err(tera::Error::message)?;
             js_to_tera_value(&JsValue::Store(JsBlob { blob }))
         }),
     );
 
     tera.register_filter(
         "unstore",
-        wrap_filter!(move(ctx) |arg, _args| {
-            let tera::Value::Object(obj) = arg else {
-                return Err("unstore must take in object".into());
-            };
-            let blob = tera_to_js_store_object(obj)?;
-            let output = ctx.load_string(&blob).map_err(|e| e.to_string())?;
-            Ok(tera::Value::String(output))
+        wrap_filter!(move(ctx) |arg: &tera::Map| {
+            let blob = tera_to_js_store_object(arg)?;
+            let output = ctx.load_string(&blob).map_err(tera::Error::message)?;
+            Ok(output.into())
         }),
     );
 
     tera.register_function(
         "zip",
         wrap_function!(move() |args| {
-            get_arg!(fst: as_array <- args);
-            get_arg!(snd: as_array <- args);
+            let fst: &[tera::Value] = args.must_get("fst")?;
+            let snd: &[tera::Value] = args.must_get("snd")?;
 
-            #[derive(Clone, Debug, Serialize, Deserialize)]
+            #[derive(Clone, Debug, Serialize)]
             struct Tuple {
                 fst: tera::Value,
                 snd: tera::Value,
@@ -320,17 +284,17 @@ fn register_functions(tera: &mut Tera, ctx: &QueryContext, writes: Arc<Mutex<Wri
 
             let output = fst.iter().cloned()
                 .zip(snd.iter().cloned())
-                .map(|(fst, snd)| serde_json::json!(Tuple { fst, snd }))
-                .collect();
-            Ok(tera::Value::Array(output))
+                .map(|(fst, snd)| Tuple { fst, snd })
+                .collect::<Vec<_>>();
+            tera::Value::try_from_serializable(&output)
         }),
     );
 }
 
 /// Resolves a path to normalized relative to the cwd
-fn resolve_path(path: &str) -> tera::Result<PathBuf> {
+fn resolve_path(path: &str) -> TeraResult<PathBuf> {
     Ok(RelativePathBuf::from_path(".")
-        .map_err(|e| e.to_string())?
+        .map_err(tera::Error::message)?
         .join_normalized(RelativePath::new(path))
         .to_path("."))
 }
@@ -341,7 +305,7 @@ fn js_to_tera_context(value: &JsValue) -> driver_util::Result<tera::Context> {
             let mut ctx = tera::Context::new();
             for (key, value) in obj.iter() {
                 let value = js_to_tera_value(value)?;
-                ctx.insert(key, &value);
+                ctx.insert(key.clone(), &value);
             }
 
             Ok(ctx)
@@ -351,96 +315,106 @@ fn js_to_tera_context(value: &JsValue) -> driver_util::Result<tera::Context> {
     }
 }
 
-fn tera_to_js_context(args: &HashMap<String, tera::Value>, ignore: &str) -> tera::Result<JsValue> {
-    let mut arg = BTreeMap::new();
+fn tera_to_js_context(args: tera::Kwargs, ignore: &str) -> TeraResult<JsValue> {
+    // TODO: this double-serialization is unavoidable because tera doesn't give us a way to iterate
+    // over the input args...
+    let mut output_args = BTreeMap::new();
     for (key, value) in args.iter() {
+        let Some(key) = key.as_str() else {
+            continue;
+        };
         if key == ignore {
             continue;
         }
         let value = tera_to_js_value(value)?;
-        let _ = arg.insert(key.clone(), value);
+        let _ = output_args.insert(key.to_string(), value);
     }
-    Ok(if arg.is_empty() {
+    Ok(if output_args.is_empty() {
         JsValue::Null
-    } else if arg.len() == 1
-        && let Some(value) = arg.get("arg")
+    } else if output_args.len() == 1
+        && let Some(value) = output_args.get("arg")
     {
         value.clone()
     } else {
-        JsValue::Object(arg)
+        JsValue::Object(output_args)
     })
 }
 
 const STORE_OBJECT_MAGIC: &str = "__store_object";
 
-fn js_to_tera_value(value: &JsValue) -> tera::Result<tera::Value> {
+fn js_to_tera_value(value: &JsValue) -> TeraResult<tera::Value> {
     Ok(match value {
-        JsValue::Undefined => tera::Value::Null,
-        JsValue::Null => tera::Value::Null,
-        JsValue::Bool(b) => tera::Value::Bool(*b),
-        JsValue::Int(i) => tera::Value::Number((*i).into()),
-        JsValue::String(s) => tera::Value::String(s.clone()),
-        JsValue::Array(arr) => {
-            tera::Value::Array(arr.iter().map(js_to_tera_value).collect::<Result<_, _>>()?)
-        }
-        JsValue::Object(obj) => tera::Value::Object(
-            obj.iter()
-                .map(|(key, value)| -> tera::Result<_> {
-                    Ok((key.clone(), js_to_tera_value(value)?))
-                })
-                .collect::<Result<_, _>>()?,
-        ),
+        JsValue::Undefined => tera::Value::undefined(),
+        JsValue::Null => tera::Value::none(),
+        JsValue::Bool(b) => (*b).into(),
+        JsValue::Int(i) => (*i).into(),
+        JsValue::String(s) => s.to_string().into(),
+        JsValue::Array(arr) => arr
+            .iter()
+            .map(js_to_tera_value)
+            .collect::<Result<Vec<_>, _>>()?
+            .as_slice()
+            .into(),
+        JsValue::Object(obj) => obj
+            .iter()
+            .map(|(key, value)| -> TeraResult<_> { Ok((key.clone(), js_to_tera_value(value)?)) })
+            .collect::<Result<HashMap<_, _>, _>>()?
+            .into(),
         JsValue::Store(s) => {
             let mut map = tera::Map::new();
-            map.insert(
-                STORE_OBJECT_MAGIC.to_string(),
-                tera::to_value(s.blob.clone())?,
-            );
-            tera::Value::Object(map)
+            let hash: &[u8] = s.blob.as_ref();
+            map.insert(STORE_OBJECT_MAGIC.into(), hash.into());
+            map.into()
         }
         JsValue::Image(_) => {
-            todo!("support passing image objects to tera")
+            unimplemented!("support passing image objects to tera")
         }
     })
 }
 
-fn tera_to_js_value(value: &tera::Value) -> tera::Result<JsValue> {
-    Ok(match value {
-        tera::Value::Null => JsValue::Null,
-        tera::Value::Bool(b) => JsValue::Bool(*b),
-        tera::Value::Number(number) => JsValue::Int(
-            number
-                .as_i64()
-                .ok_or("can only take i32")?
-                .try_into()
-                .map_err(|_| "can only take i32")?,
-        ),
-        tera::Value::String(s) => JsValue::String(s.clone()),
-        tera::Value::Array(arr) => {
-            JsValue::Array(arr.iter().map(tera_to_js_value).collect::<Result<_, _>>()?)
-        }
-        tera::Value::Object(obj) => match tera_to_js_store_object(obj) {
+fn tera_to_js_value(value: &tera::Value) -> TeraResult<JsValue> {
+    Ok(if value.is_undefined() {
+        JsValue::Undefined
+    } else if value.is_none() {
+        JsValue::Null
+    } else if let Some(b) = value.as_bool() {
+        JsValue::Bool(b)
+    } else if let Some(i) = value.as_i64() {
+        JsValue::Int(i as i32)
+    } else if let Some(s) = value.as_str() {
+        JsValue::String(s.to_string())
+    } else if let Some(arr) = value.as_array() {
+        JsValue::Array(arr.iter().map(tera_to_js_value).collect::<Result<_, _>>()?)
+    } else if let Some(obj) = value.as_map() {
+        match tera_to_js_store_object(obj) {
             Ok(blob) => JsValue::Store(JsBlob { blob }),
             Err(_) => JsValue::Object(
                 obj.into_iter()
-                    .map(|(key, value)| -> tera::Result<_> {
-                        Ok((key.clone(), tera_to_js_value(value)?))
+                    .filter_map(|(key, value)| -> Option<TeraResult<_>> {
+                        let key = key.as_str()?.to_string();
+                        let value = tera_to_js_value(value);
+                        Some(value.map(|value| (key, value)))
                     })
                     .collect::<Result<_, _>>()?,
             ),
-        },
+        }
+    } else {
+        return Err(tera::Error::message("unsupported argument type"));
     })
 }
 
-fn tera_to_js_store_object(obj: &tera::Map<String, tera::Value>) -> tera::Result<Blob> {
-    match obj.get(STORE_OBJECT_MAGIC) {
-        Some(hash) => {
-            let hash = tera::from_value(hash.clone())?;
-            // SAFETY: we have no choice but to trust this. The user has purposefully messed us
-            // up otherwise, worst case we will find there is no backing file.
-            let blob = unsafe { Blob::from_hash(hash) };
-            Ok(blob)
-        }
-        None => Err("not a store object".into()),
-    }
+fn tera_to_js_store_object(obj: &tera::Map) -> TeraResult<Blob> {
+    let hash = obj
+        .get(&STORE_OBJECT_MAGIC.into())
+        .ok_or_else(|| tera::Error::message("not a store object"))?;
+    let hash = hash
+        .as_bytes()
+        .ok_or_else(|| tera::Error::message("magic value wasn't bytes"))?
+        .try_into()
+        .map_err(|_| tera::Error::message("invalid byte length"))?;
+
+    // SAFETY: we have no choice but to trust this. The user has purposefully messed us
+    // up otherwise, worst case we will find there is no backing file.
+    let blob = unsafe { Blob::from_hash(hash) };
+    Ok(blob)
 }
